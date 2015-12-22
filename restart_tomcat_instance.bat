c19f9238c102df4125bbe4ac666dd5b44dc71d34
Julian Rivera <xtecuan@gmail.com>
2015-12-22 08:02:24 -0500

    sample restart_tomcat_instance.bat

--- restart_tomcat_instance.bat
+++ restart_tomcat_instance.bat
@@ -1,15 +1,24 @@
 @echo off
+set tomcat_scripts=C:\Users\julianr\Desktop\tomcat-related-scripts
 set git_bash_path="D:\Program_Files\Git\bin"
 set PATH=%PATH%;%git_bash_path%
 
 set instanceName=%1
 
-sh -i catpid.sh %instanceName% > %instanceName%.txt
 
-set /P cpid=< %instanceName%.txt
+
+
+sh -i %tomcat_scripts%\catpid.sh %instanceName% > %tomcat_scripts%\%instanceName%.txt
+
+set /P cpid=< %tomcat_scripts%\%instanceName%.txt
+
+tasklist /FI "PID eq %cpid%"
 
 echo Killing %cpid%
 
-rem runas /profile /env /user:idb\julianr "taskkill /F /PID %cpid%"
 
-taskkill /F /PID %cpid%
+rem runas /user:idb\jr-idb "taskkill /F /PID %cpid%"
+
+rem taskkill /F /PID %cpid%
+
+pause
